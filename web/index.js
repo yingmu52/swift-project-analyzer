cd323dc0b7e6a9dc4f1d8336e8fdc69b605cb571
Xinyi <yingmu52@msn.com>
2021-07-28 12:07:55 -0700

    wip

--- web/index.js
+++ web/index.js
@@ -1,55 +1,53 @@
 // see https://github.com/d3/d3-force
 
-drag = (simulation) => {
+const drag = simulation => {
+  
   function dragstarted(event, d) {
     if (!event.active) simulation.alphaTarget(0.3).restart();
     d.fx = d.x;
     d.fy = d.y;
   }
-
-  function dragged(event, d) {
+  
+  function dragged(event,d) {
     d.fx = event.x;
     d.fy = event.y;
   }
-
-  function dragended(event, d) {
+  
+  function dragended(event,d) {
     if (!event.active) simulation.alphaTarget(0);
     d.fx = null;
     d.fy = null;
   }
-
-  return d3
-    .drag()
-    .on("start", dragstarted)
-    .on("drag", dragged)
-    .on("end", dragended);
-};
-var height = 600;
-var width = 600;
-
-d3.json("web/data.json").then((data) => {
   
-  const root = d3.hierarchy(data);
-  const links = root.links();
-  const nodes = root.descendants();
+  return d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+}
 
-  const simulation = d3
-    .forceSimulation(nodes)
-    .force(
-      "link",
-      d3
-        .forceLink(links)
-        .id((d) => d.id)
-        .distance(0)
-        .strength(1)
-    )
-    .force("charge", d3.forceManyBody().strength(-50))
-    .force("x", d3.forceX())
-    .force("y", d3.forceY());
+const color = d => {
+  const scale = d3.scaleOrdinal(d3.schemeCategory10);
+  return d => scale(d.group);
+}
 
-  const svg = d3
-    .create("svg")
-    .attr("viewBox", [-width / 2, -height / 2, width, height]);
+// const svg = d3.select("svg")
+// const width = +svg.attr("width")
+// const height = +svg.attr("height")
+
+const width = 960, height = 960;
+
+d3.json("web/disjoint.json").then(data => {
+  const links = data.links.map(d => Object.create(d));
+  const nodes = data.nodes.map(d => Object.create(d));
+
+  const simulation = d3.forceSimulation(nodes)
+      .force("link", d3.forceLink(links).id(d => d.id))
+      .force("charge", d3.forceManyBody())
+      .force("x", d3.forceX())
+      .force("y", d3.forceY());
+
+  const svg = d3.select("svg")
+      .attr("viewBox", [-width / 2, -height / 2, width, height]);
 
   const link = svg
     .append("g")
@@ -57,34 +55,34 @@ d3.json("web/data.json").then((data) => {
     .attr("stroke-opacity", 0.6)
     .selectAll("line")
     .data(links)
-    .join("line");
+    .join("line")
+    .attr("stroke-width", (d) => Math.sqrt(d.value));
 
   const node = svg
     .append("g")
-    .attr("fill", "#fff")
-    .attr("stroke", "#000")
+    .attr("stroke", "#fff")
     .attr("stroke-width", 1.5)
     .selectAll("circle")
     .data(nodes)
     .join("circle")
-    .attr("fill", (d) => (d.children ? null : "#000"))
-    .attr("stroke", (d) => (d.children ? null : "#fff"))
-    .attr("r", 3.5)
+    .attr("r", 5)
+    .attr("fill", color)
     .call(drag(simulation));
+    
 
-  node.append("title").text((d) => d.data.name);
+
+  node.append("title")
+      .text(d => d.id);
 
   simulation.on("tick", () => {
     link
-      .attr("x1", (d) => d.source.x)
-      .attr("y1", (d) => d.source.y)
-      .attr("x2", (d) => d.target.x)
-      .attr("y2", (d) => d.target.y);
+        .attr("x1", d => d.source.x)
+        .attr("y1", d => d.source.y)
+        .attr("x2", d => d.target.x)
+        .attr("y2", d => d.target.y);
 
-    node.attr("cx", (d) => d.x).attr("cy", (d) => d.y);
+    node
+        .attr("cx", d => d.x)
+        .attr("cy", d => d.y);
   });
-
-  invalidation.then(() => simulation.stop());
-  console.log(invalidation);
-  console.log(svg.node())
-});
+})
